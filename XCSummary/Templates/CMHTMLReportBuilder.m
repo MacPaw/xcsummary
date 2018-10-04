96d6b502ba0a043394e4303e83fa82dece30e45e
Volodymyr Borodavchuk <vborodav@macpaw.com>
2018-10-04 11:41:45 +0300

    Added parsing of Screenshot name to avoid extension issues

--- XCSummary/Templates/CMHTMLReportBuilder.m
+++ XCSummary/Templates/CMHTMLReportBuilder.m
@@ -137,12 +137,13 @@
     if (activity.hasScreenshotData)
     {
         templateFormat = [self _decodeTemplateWithName:ActivityTemplateWithImage];
-        NSString *imageName = [NSString stringWithFormat:@"Screenshot_%@.png", activity.uuid.UUIDString];
+        CMAttachment *attachment = [activity.attachments firstObject];
+        NSString *imageName = [NSString stringWithFormat:@"%@", attachment.filename];
         NSString *fullPath = [self.path stringByAppendingPathComponent:imageName];
         
         [self.fileManager copyItemAtPath:fullPath toPath:[self.htmlResourcePath stringByAppendingPathComponent:imageName] error:nil];
         
-        NSString *localImageName = [NSString stringWithFormat:@"resources/Screenshot_%@.png", activity.uuid.UUIDString];
+        NSString *localImageName = [NSString stringWithFormat:@"resources/%@", imageName];
         composedString = [NSString stringWithFormat:templateFormat, indentation, @"px", activity.title, activity.finishTimeInterval - activity.startTimeInterval, localImageName];
     }
     else
